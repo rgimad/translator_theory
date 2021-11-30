6299413c52f767c22964aa74f33cb51a796a273a
rgimad <rustem.gimadutdinov@gmail.com>
2021-11-30 12:22:25 +0300

    Add project files.

---
+++ lang1/lang1.cpp
@@ -0,0 +1,404 @@
+/*
+		Interpreter of small programming language.
+		I has following syntax: {} - 0 or more, [] - 0 or 1, () - grouping
+
+		program            := {statement}
+		statement          := assign | condition | compound_statement
+		compound_statement := "{" {statement} "}"
+		assign             := id "=" expr ";"
+		condition          := "if" "(" expr ")" statement ["else" statement]
+		expr               := operand {op operand}
+		operand            := ["-"] (id | number)
+		op                 := "+" | "-" | "*" | "/" | "==" | "!=" | ">" | "<" | "<=" | ">="
+		number             := [0-9]+
+		id                 := [a-zA-Z][a-zA-Z_0-9]*
+*/
+#include <cassert>
+#include <cctype>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <variant>
+#include "magic_enum.hpp"
+
+enum class TokenType {
+	IF,
+	ELSE,
+	ID,
+	NUMBER,
+	SEMICOLON,
+	LPAREN,
+	RPAREN,
+	LCURLY,
+	RCURLY,
+	PLUS,
+	MINUS,
+	STAR,
+	SLASH,
+	EQUAL,      // ==
+	NOT_EQUAL,  // !=
+	ASSIGN,     // =
+	GREATER,
+	GREATER_EQUAL,
+	LESS,
+	LESS_EQUAL,
+	ENDOFFILE
+};
+
+enum class AstNodeType {
+	PROGRAM,
+	COMPOUND_ST,
+	CONDITION,
+	ASSIGN,
+	BINOP,
+	VAR,
+	NUMBER
+};
+
+struct Token {
+	TokenType type;
+	std::variant<int, std::string> value;
+
+	Token() { }
+	Token(TokenType type, int val) : type(type), value(val) {}
+	Token(TokenType type, std::string& val) : type(type), value(val) {}
+	Token(const Token& t) {
+		type = t.type;
+		value = t.value;
+	}
+	~Token() {}
+};
+
+struct AstNode {
+	AstNodeType type;
+	bool unary_minus; // with unary minus or not
+	std::variant<int, std::string> value;
+	std::vector<AstNode*> children;
+
+	void push_child(AstNode* child) { children.push_back(child); }
+
+	static void showAst(AstNode *root, int level = 0) {
+		for (int i = 0; i < level; i++) {
+			std::cout << "    ";
+		}
+		std::cout << magic_enum::enum_name(root->type) << "\n";
+		for (AstNode *child : root->children) {
+			showAst(child, level + 1);
+		}
+	}
+
+	AstNode(AstNodeType type) : type(type), unary_minus(false) {}
+	~AstNode() {};
+};
+
+std::string token_to_string(const Token& token) {
+	std::string str;
+	str += "{ type = ";
+	if (token.type == TokenType::ID) {
+		str += ("ID; value = " + std::get<std::string>(token.value) + "; }");
+	}
+	else if (token.type == TokenType::NUMBER) {
+		str += (std::string("NUMBER; value = ") + std::to_string(std::get<int>(token.value)) + "; }");
+	}
+	else {
+		str += magic_enum::enum_name(token.type);
+		str += "; value = None; }";
+	}
+	return str;
+}
+
+std::vector<Token> tokenize(const std::string& text) {
+	std::vector<Token> tokens;
+	size_t pos = 0, text_len = text.length();
+	while (pos < text_len) {
+		char c = text[pos++];
+		if (isalpha(c) || c == '_') {
+			std::string word;
+			while (pos < text_len && (isalpha(c) || c == '_' || isdigit(c))) {
+				word += c;
+				c = text[pos++];
+			}
+			pos--;
+			if (word == "if") {
+				tokens.push_back(Token(TokenType::IF, word));
+			}
+			else if (word == "else") {
+				tokens.push_back(Token(TokenType::ELSE, word));
+			}
+			else {
+				tokens.push_back(Token(TokenType::ID, word));
+			}
+		}
+		else if (isdigit(c)) {
+			int num = 0;
+			while (pos < text_len && isdigit(c)) {
+				num = num * 10 + (c - '0');
+				c = text[pos++];
+			}
+			pos--;
+			tokens.push_back(Token(TokenType::NUMBER, num));
+		}
+		else if (c == '=') {
+			if (pos < text_len && text[pos] == '=') {
+				pos++;
+				tokens.push_back(Token(TokenType::EQUAL, 0));
+			}
+			else {
+				tokens.push_back(Token(TokenType::ASSIGN, 0));
+			}
+		}
+		else if (c == '!') {
+			if (pos < text_len && text[pos] == '=') {
+				pos++;
+				tokens.push_back(Token(TokenType::NOT_EQUAL, 0));
+			}
+			else {
+				throw std::string("Unexpected token: !");
+			}
+		}
+		else if (c == '<') {
+			if (pos < text_len && text[pos] == '=') {
+				pos++;
+				tokens.push_back(Token(TokenType::LESS_EQUAL, 0));
+			}
+			else {
+				tokens.push_back(Token(TokenType::LESS, 0));
+			}
+		}
+		else if (c == '>') {
+			if (pos < text_len && text[pos] == '=') {
+				pos++;
+				tokens.push_back(Token(TokenType::GREATER_EQUAL, 0));
+			}
+			else {
+				tokens.push_back(Token(TokenType::GREATER, 0));
+			}
+		}
+		else if (c == ';') {
+			tokens.push_back(Token(TokenType::SEMICOLON, 0));
+		}
+		else if (c == '(') {
+			tokens.push_back(Token(TokenType::LPAREN, 0));
+		}
+		else if (c == ')') {
+			tokens.push_back(Token(TokenType::RPAREN, 0));
+		}
+		else if (c == '{') {
+			tokens.push_back(Token(TokenType::LCURLY, 0));
+		}
+		else if (c == '}') {
+			tokens.push_back(Token(TokenType::RCURLY, 0));
+		}
+		else if (c == '+') {
+			tokens.push_back(Token(TokenType::PLUS, 0));
+		}
+		else if (c == '-') {
+			tokens.push_back(Token(TokenType::MINUS, 0));
+		}
+		else if (c == '*') {
+			tokens.push_back(Token(TokenType::STAR, 0));
+		}
+		else if (c == '/') {
+			tokens.push_back(Token(TokenType::SLASH, 0));
+		}
+		else if (c != '\n' && c != '\r' && c != ' ') {
+			throw std::string("Unexpected token: ") + c;
+		}
+	}
+	tokens.push_back(Token(TokenType::ENDOFFILE, 0));
+	return tokens;
+}
+
+bool token_is_op(const Token &t) {
+	return (t.type == TokenType::PLUS ||
+		t.type == TokenType::MINUS ||
+		t.type == TokenType::STAR ||
+		t.type == TokenType::SLASH ||
+		t.type == TokenType::EQUAL ||
+		t.type == TokenType::NOT_EQUAL ||
+		t.type == TokenType::GREATER ||
+		t.type == TokenType::GREATER_EQUAL ||
+		t.type == TokenType::LESS ||
+		t.type == TokenType::LESS_EQUAL
+	);
+}
+
+class Parser {
+private:
+	std::vector<Token> tokens;
+	size_t pos;
+
+public:
+	Parser(std::vector<Token>&& t) : tokens(std::move(t)), pos(0) {}
+	~Parser() {}
+
+	AstNode* parse_operand() {
+		std::cout << "parse_operand()\n";
+		bool unary_minus = false;
+		if (tokens[pos].type == TokenType::MINUS) {
+			unary_minus = true;
+			pos++;
+		}
+		if (tokens[pos].type == TokenType::ID) {
+			AstNode* operand = new AstNode(AstNodeType::VAR);
+			operand->unary_minus = unary_minus;
+			operand->value = std::get<std::string>(tokens[pos].value);
+			pos++;
+			return operand;
+		}
+		else if (tokens[pos].type == TokenType::NUMBER) {
+			AstNode* operand = new AstNode(AstNodeType::NUMBER);
+			operand->unary_minus = unary_minus;
+			operand->value = std::get<int>(tokens[pos].value);
+			pos++;
+			return operand;
+		}
+		else {
+			return nullptr;
+			// throw std::string("Syntax error: expected ID or NUMBER");
+		}
+	}
+
+	AstNode* parse_expr() {
+		std::cout << "parse_expr()"/* << magic_enum::enum_name(tokens[pos].type)*/ <<  "\n";
+		AstNode* operand1 = parse_operand();
+		if (operand1 == nullptr) {
+			throw std::string("Syntax error: expected expression\n");
+		}
+		if (token_is_op(tokens[pos])) {
+			AstNode* operand; int op;
+			while (token_is_op(tokens[pos])) {
+				op = (int)tokens[pos].type;
+				pos++;
+				operand = parse_operand();
+				if (operand == nullptr) {
+					throw std::string("Syntax error: expected operand\n");
+				}
+				AstNode* binop = new AstNode(AstNodeType::BINOP);
+				binop->value = op;
+				binop->push_child(operand1);
+				binop->push_child(operand);
+				operand1 = binop;
+			}
+		}
+		return operand1;
+	}
+
+	AstNode* parse_statement() {
+		std::cout << "parse_statement()\n";
+		if (tokens[pos].type == TokenType::ID && tokens[pos + 1].type == TokenType::ASSIGN) {
+			
+			AstNode* assign = new AstNode(AstNodeType::ASSIGN);
+			AstNode* lval = new AstNode(AstNodeType::VAR);
+			lval->value = std::get<std::string>(tokens[pos].value);
+			assign->push_child(lval);
+			pos += 2;
+			assign->push_child(parse_expr());
+			if (tokens[pos].type != TokenType::SEMICOLON) {
+				throw std::string("Syntax error: expected ;");
+			}
+			pos++;
+			return assign;
+		}
+		else if (tokens[pos].type == TokenType::IF) {
+			AstNode* cond = new AstNode(AstNodeType::CONDITION);
+			if (tokens[pos + 1].type != TokenType::LPAREN) {
+				throw std::string("Syntax error: expected (");
+			}
+			pos += 2;
+			// std::cout << "cond->push_child(parse_expr())...\n";
+			cond->push_child(parse_expr());
+			// std::cout << "after cond->push_child(parse_expr())\n";
+			if (tokens[pos].type != TokenType::RPAREN) {
+				throw std::string("Syntax error: expected )");
+			}
+			pos++;
+			cond->push_child(parse_statement());
+			if (tokens[pos].type == TokenType::ELSE) {
+				pos++;
+				cond->push_child(parse_statement());
+			}
+			return cond;
+		}
+		else if (tokens[pos].type == TokenType::LCURLY) {
+			pos++;
+			AstNode* compound = new AstNode(AstNodeType::COMPOUND_ST);
+			AstNode* stat;
+			while ((stat = parse_statement()) != nullptr) {
+				compound->push_child(stat);
+			}
+			if (tokens[pos].type != TokenType::RCURLY) {
+				throw std::string("Syntax error: expected }");
+			}
+			return compound;
+		}
+		return nullptr;
+	}
+
+	AstNode* parse_program() {
+		std::cout << "parse_program()\n";
+		AstNode* program = new AstNode(AstNodeType::PROGRAM);
+		AstNode* stat;
+		while ((stat = parse_statement()) != nullptr) {
+			program->push_child(stat);
+		}
+		return program;
+	}
+};
+
+/*
+		Receives program code, runs it
+		Returns state of variables after running program
+*/
+std::map<std::string, int> eval(const std::string& program) {
+	Parser parser(std::move(tokenize(program)));
+	AstNode* root = parser.parse_program();
+	return { {"aboba", 1337} };
+}
+
+/*
+		Runs program and compares its result with given state
+*/
+bool run_test(const std::string& program,
+			  const std::map<std::string, int>& needed_variable_state) {
+	auto result_state = eval(program);
+	if (result_state.size() != needed_variable_state.size()) {
+		return false;
+	}
+	for (const auto& item : needed_variable_state) {
+		if (result_state[item.first] != item.second) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	// assert(run_test(std::string("x = 1337;"), {{"x", 1337}}));
+	// std::cout << "TESTS PASSED\n";
+
+	auto tokens = tokenize(std::string(
+		"myvar1=123+7*9;"
+		"ans=0;"
+		"if(myvar1==63+123)"
+		"ans=1;"
+		"if (1==1) {if (2>1) a = 7; else b = 9;}"
+	));
+	for (const Token& t : tokens) {
+		std::cout << token_to_string(t) << "\n";
+	}
+
+	Parser parser(std::move(tokens));
+	try {
+		AstNode* root = parser.parse_program();
+		// std::cout << magic_enum::enum_name(root->children[0]->children[0]->type);
+		std::cout << "AST:\n";
+		AstNode::showAst(root);
+	}
+	catch (const std::string &s) {
+		std::cout << s;
+	}
+	
+	
+}
